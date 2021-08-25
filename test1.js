068c1628d0c174ced6fec8ace8c2d6811bba0c03
SamyakJain2020 <samyakjainking@gmail.com>
2021-08-25 23:50:03 +0530

    commit from SAM

---
+++ test1.js
@@ -0,0 +1,8 @@
+console.log("osdfbjesdf");
+addBtn=document.getElementById("addbtn");
+addBtn.addEventListener("click",()=>{
+//   let title=document.getItem("title");
+//   let desc=document.getItem("desc");
+console.log("Updating sfmslfm");
+//   alert(title+" "+desc);
+})
