adecfbd97def7d652681771e66dde5c6b727bc18
Sanchita Mishra <59258203+SanchitaMishra170676@users.noreply.github.com>
2021-08-31 20:03:29 +0530

    deleting extra docs

--- test1.js
+++
@@ -1,8 +0,0 @@
-console.log("osdfbjesdf");
-addBtn=document.getElementById("addbtn");
-addBtn.addEventListener("click",()=>{
-//   let title=document.getItem("title");
-//   let desc=document.getItem("desc");
-console.log("Updating sfmslfm");
-//   alert(title+" "+desc);
-})
